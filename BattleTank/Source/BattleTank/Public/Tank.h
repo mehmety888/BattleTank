c77583a5749e580b526b1dd68643d8b20d3f010a
Mehmet Yıldırım <mehmet.yildirim@avl.com>
2020-05-30 17:59:08 +0300

    feat: set launchspeed parameter

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -34,4 +34,9 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+
+	UPROPERTY(EditAnywhere, Category = Firing)
+	float LaunchSpeed = 100000.f; // 1000 m/s TODO: find a sensible default
+
 };
