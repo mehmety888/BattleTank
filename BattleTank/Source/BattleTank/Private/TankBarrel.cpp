2ef955f54d3970632977f2eac5d8e09f523abfd1
Mehmet Yıldırım <mehmet.yildirim@avl.com>
2020-05-31 17:04:40 +0300

    fix: bug occured because of TankBarrel

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -7,5 +7,9 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 {
     // Move the barrel right amount this frame
 	// Given a max elevation speed, and the frame time
-
+    RelativeSpeed = FMath::Clamp<float>(RelativeSpeed,-1.f,1.f);
+    auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+    auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+    auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+    SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
