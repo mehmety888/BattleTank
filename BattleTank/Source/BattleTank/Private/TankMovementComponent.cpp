94850d0d2f3d5f9946e93439877e0134c489190e
Mehmet Yıldırım <mehmet.yildirim@avl.com>
2020-06-03 15:21:16 +0300

    feat: implement IntendMoveForward method

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,8 +2,25 @@
 
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+    if(!LeftTrackToSet || !RightTrackToSet)
+    {
+        return;
+    }
+
+    LeftTrack = LeftTrackToSet;
+    RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
     UE_LOG(LogTemp, Warning, TEXT("Intend Move Forword : %f"),Throw);
+
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(Throw);
+
+    // Prevent double speed due to dual control use
 }
